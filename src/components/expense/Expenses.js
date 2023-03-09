00f1ea0dc1e48784f369b97a02001da79ae4e036
s11verz <72031018+s11verz@users.noreply.github.com>
2023-03-09 16:33:46 +0900

    syntax

--- src/components/expense/Expenses.js
+++ src/components/expense/Expenses.js
@@ -2,32 +2,32 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../card/Card";
 import "../../css/Expenses.css";
 
-function Expenses(props) {
-    console.log(props);
+const Expenses = (props) => {
+  console.log(props);
   return (
     <Card className="expenses">
-    <ExpenseItem
-      title={props.items[0].title}
-      date={props.items[0].date}
-      amount={props.items[0].amount}
-    ></ExpenseItem>
-    <ExpenseItem
-      title={props.items[1].title}
-      amount={props.items[1].amount}
-      date={props.items[1].date}
-    ></ExpenseItem>
-    <ExpenseItem
-      title={props.items[2].title}
-      amount={props.items[2].amount}
-      date={props.items[2].date}
-    ></ExpenseItem>
-    <ExpenseItem
-      title={props.items[3].title}
-      amount={props.items[3].amount}
-      date={props.items[3].date}
-    ></ExpenseItem>
-  </Card>
-    );
-}
+      <ExpenseItem
+        title={props.items[0].title}
+        date={props.items[0].date}
+        amount={props.items[0].amount}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      ></ExpenseItem>
+    </Card>
+  );
+};
 
 export default Expenses;
