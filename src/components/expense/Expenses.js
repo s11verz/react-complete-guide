12fcfc7b49adb26be9efc045c97266fdfb1c48c2
s11verz <72031018+s11verz@users.noreply.github.com>
2023-03-10 13:52:00 +0900

    list 다루기

--- src/components/expense/Expenses.js
+++ src/components/expense/Expenses.js
@@ -1,44 +1,34 @@
-
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpenseFilter";
 import Card from "../card/Card";
 import "../../css/Expenses.css";
 
 const Expenses = (props) => {
-
-  const [filteredYear, setFilteredYear] = useState('2022');
+  const [filteredYear, setFilteredYear] = useState("2022");
 
   const changeFilterDataHandler = (changedFilterData) => {
     setFilteredYear(changedFilterData);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter
-        selected={ filteredYear}
+        selected={filteredYear}
         onChangeFilterData={changeFilterDataHandler}
       ></ExpensesFilter>
-      <ExpenseItem
-        title={props.items[0].title}
-        date={props.items[0].date}
-        amount={props.items[0].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      ></ExpenseItem>
+      {filteredExpenses.map((expense, index) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          date={expense.date}
+          amount={expense.amount}
+        ></ExpenseItem>
+      ))}
     </Card>
   );
 };
